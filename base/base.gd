b5be9abfc1b2999a01e86ddc8c2cfa6e6401c4e1
Benedikt Schnatterbeck <benediktms@hey.com>
2024-06-10 17:52:59 +0100

    refator: use getters and setters for health

--- base/base.gd
+++ base/base.gd
@@ -3,7 +3,14 @@ class_name Base
 extends Node3D
 
 @export var max_health := 10
-@export var current_health: int
+var current_health: int:
+	set(new_value):
+		current_health = new_value
+		label_3d.text = str(new_value)
+		if current_health < 1:
+			get_tree().reload_current_scene()
+	get:
+		return current_health
 
 @onready var label_3d: Label3D = $Label3D
 
@@ -11,7 +18,6 @@ var path: Path3D
 var target_pos: Vector3
 
 func _ready() -> void:
-	label_3d.text = str(max_health)
 	current_health = max_health
 
 func _enter_tree() -> void:
@@ -32,8 +38,4 @@ func _process(_delta: float) -> void:
 		global_position = path.curve.get_point_position(path.curve.point_count - 1)
 
 func take_damage() -> void:
-	if current_health > 0:
-		current_health -= 1
-		label_3d.text = str(current_health)
-	else:
-		print("game over")
+	current_health -= 1

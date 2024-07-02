57f2a018aa526a5df0be1645b245795b5bf8dabf
Benedikt Schnatterbeck <benediktms@hey.com>
2024-07-02 22:25:30 +0100

    feat: add initial setup for currency UI

---
+++ Bank.cs
@@ -0,0 +1,40 @@
+using Godot;
+using System;
+
+namespace BarbarianBlaster;
+
+public partial class Bank : MarginContainer
+{
+    private Label? _label;
+
+    private Label Label
+    {
+        get
+        {
+            if (_label is not null) return _label;
+
+            _label = GetNode<Label>("Label");
+            return _label;
+        }
+        set => _label = value;
+    }
+
+    private int _gold;
+
+    private int Gold
+    {
+        get => _gold;
+        set
+        {
+            _gold = Math.Max(value, 0);
+            Label.Text = $"Gold: {_gold}";
+        }
+    }
+
+    public override void _Ready()
+    {
+        base._Ready();
+
+        Gold = 150;
+    }
+}

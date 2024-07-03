1430349440911f7ffd9924c23b2c6a31ed4fd024
Benedikt Schnatterbeck <benediktms@hey.com>
2024-07-03 23:44:42 +0100

    refactor: simplify enemy script

--- enemy/Enemy.cs
+++ enemy/Enemy.cs
@@ -5,51 +5,16 @@ namespace BarbarianBlaster;
 
 public partial class Enemy : PathFollow3D
 {
-    [Export] private float _speed = 5;
+    private Base _homeBase = null!;
 
-    private Base? _homeBase;
+    private AnimationPlayer _animationPlayer = null!;
 
-    private Base HomeBase
-    {
-        get
-        {
-            if (_homeBase is not null) return _homeBase;
-            _homeBase = (Base)GetTree().GetFirstNodeInGroup("base");
-            return _homeBase;
-        }
-    }
-
-    private AnimationPlayer? _animationPlayer;
-
-    private AnimationPlayer AnimationPlayer
-    {
-        get
-        {
-            if (_animationPlayer is not null) return _animationPlayer;
-            _animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
-            return _animationPlayer;
-        }
-    }
-
-    private int _reward = 15;
-    private Bank? _bank;
-
-    private Bank Bank
-    {
-        get
-        {
-            if (_bank is not null) return _bank;
-
-            _bank = (Bank)GetTree().GetFirstNodeInGroup("bank");
-            return _bank;
-        }
-    }
+    private Bank _bank = null!;
 
     public int? MaxHealth { get; set; }
 
     private int _currentHealth;
 
-    [Export]
     public int CurrentHealth
     {
         get => _currentHealth;
@@ -57,21 +22,29 @@ public partial class Enemy : PathFollow3D
         {
             if (value < _currentHealth)
             {
-                AnimationPlayer.Play("TakeDamage");
+                _animationPlayer.Play("TakeDamage");
             }
 
             _currentHealth = value;
 
             if (_currentHealth <= 0)
             {
-                Bank.Gold += _reward;
+                _bank.Gold += _reward;
                 QueueFree();
             }
         }
     }
 
+    private int _reward = 15;
+
+    private float _speed = 5;
+
     public override void _Ready()
     {
+        _animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+        _homeBase = (Base)GetTree().GetFirstNodeInGroup("base");
+        _bank = (Bank)GetTree().GetFirstNodeInGroup("bank");
+
         CurrentHealth = MaxHealth ?? 3; // MaxHealth should always be set in EnemyPath
     }
 
@@ -80,7 +53,7 @@ public partial class Enemy : PathFollow3D
         Progress += (float)delta * _speed;
         if (Math.Abs(ProgressRatio - 1.0) < 0.01)
         {
-            HomeBase.TakeDamage();
+            _homeBase.TakeDamage();
             SetProcess(false);
             QueueFree();
         }
